14f94feb989abe513a10d46de742cae5f3ca4492
VFedyaev <VFedyaev@stat.kg>
2018-12-27 15:09:27 +0600

    hide nav

--- Questionnaire/Questionnaire.WEB/Scripts/app.js
+++ Questionnaire/Questionnaire.WEB/Scripts/app.js
@@ -8,7 +8,7 @@ function menuInit() {
         $("#sidebar").mCustomScrollbar({
             theme: "minimal"
         });
-
+ 
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar, #content, .navbar').toggleClass('active');
             $('.collapse.in').toggleClass('in');
@@ -16,7 +16,14 @@ function menuInit() {
         });
     });
 }
-
+window.onload = function () {
+    var url = window.location.href.toLowerCase();
+    if (url.indexOf('formdata') >= 0) {
+        $('#sidebar, #content, .navbar').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    }
+}
 function activeMenuItem() {
     turnOffCurrentActiveMenuItem();
 
