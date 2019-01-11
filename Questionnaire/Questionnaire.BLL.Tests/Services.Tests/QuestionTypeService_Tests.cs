1985d806fddd55d5e0a8472456dd43907296cec8
VFedyaev <VFedyaev@stat.kg>
2019-01-11 16:39:57 +0600

    fix delete into Devision and Forms

--- Questionnaire/Questionnaire.BLL.Tests/Services.Tests/QuestionTypeService_Tests.cs
+++ Questionnaire/Questionnaire.BLL.Tests/Services.Tests/QuestionTypeService_Tests.cs
@@ -5,15 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using System.Web.ModelBinding;
+using Questionnaire.BLL.Services;
+using Questionnaire.DAL.Repositories;
+using Questionnaire.BLL.Interfaces;
 
 namespace Questionnaire.BLL.Tests.Services.Tests
 {
     [TestClass]
     public class QuestionTypeService_Tests : BaseInit
     {
+        private Mock<UnitOfWork> _mockRepository;
+        private ModelStateDictionary _modelState;
+        private IQuestionTypeService _service;
+
         [TestInitialize]
         public void TestInitialize()
         {
+            //_mockRepository = new Mock<UnitOfWork>();
+            //_modelState = new ModelStateDictionary();
+            //_service = new QuestionTypeService(new ModelStateWrapper(_modelState), _mockRepository.Object);
             base.QuestionTypeInitialize();
         }
 
@@ -27,7 +39,7 @@ namespace Questionnaire.BLL.Tests.Services.Tests
         //    //Assert
         //}
         [TestMethod]
-        public void Create_FiledsNotEmptyAddElementIntoDB_CreateIsOk()
+        public void Create_TryToAddElementIntoDbWithNotEmptyFileds_ReturnsTrue()
         {
             //Arrange
             var item = new QuestionTypeDTO { Name = $"New-{DateTime.Now}" };
@@ -35,6 +47,19 @@ namespace Questionnaire.BLL.Tests.Services.Tests
             QuestionTypeService.Add(item);
             //Assert
             Assert.IsTrue(moqQuestionTypeRepository.Items.Any(t=>t.Name == item.Name));
+        }
+
+        [TestMethod]
+        public void Create_TryToAddElementIntoDbWithEmptyFileds_ReturnsRequired()
+        {
+            //Arrange
+            var item = new QuestionTypeDTO { Name = "" };
+            //Act
+            QuestionTypeService.Add(item);
+            //Assert
+            Assert.IsTrue(moqQuestionTypeRepository.Items.Any(t => t.Name == item.Name));
+            var error = _modelState["Name"].Errors[0];
+            Assert.AreEqual("Заполните поле!", error.ErrorMessage);
         }
     }
 }
